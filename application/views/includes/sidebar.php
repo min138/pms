116c37ca0abbcfba5ca4ee6720e8682e3a982dcd
min138 <mineshmamrawala5@gmail.com>
2015-10-08 09:00:08 +0530

    SideBars

---
+++ application/views/includes/sidebar.php
@@ -0,0 +1,52 @@
+<?php
+$class = strtolower($this->router->fetch_class());
+?>
+<div class="page-sidebar-wrapper">
+    <div class="page-sidebar navbar-collapse collapse">
+        <!-- BEGIN SIDEBAR MENU -->
+        <!-- DOC: for circle icon style menu apply page-sidebar-menu-circle-icons class right after sidebar-toggler-wrapper -->
+        <ul class="page-sidebar-menu">
+            <li class="sidebar-toggler-wrapper">
+                <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+                <div class="sidebar-toggler">
+                </div>
+                <div class="clearfix">
+                </div>
+                <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+            </li>
+            <li class="sidebar-search-wrapper">
+                <form class="search-form" role="form" action="index.html" method="get">
+                    <div class="input-icon right">
+                        <i class="icon-magnifier"></i>
+                        <input type="text" class="form-control" name="query" placeholder="Search...">
+                    </div>
+                </form>
+            </li>
+            <li class="start <?php echo $class == "dashboard" ? "active" : ''; ?> ">
+                <a href="<?php echo base_url('dashboard'); ?>">
+                    <i class="icon-home"></i>
+                    <span class="title">Dashboard</span>
+                    <span class="selected"></span>
+                </a>
+            </li>
+            <li class="<?php echo $class == "employee" ? "active" : ''; ?>">
+                <a href="javascript:;">
+                    <i class="icon-users"></i>
+                    <span class="title">Employee</span>
+                    <span class="arrow "></span>
+                </a>
+                <ul class="sub-menu">
+                    <li class="<?php echo $active_tab == "employee" ? "active" : ''; ?>">
+                        <a href="<?php echo base_url('employee'); ?>">
+                            <i class="icon-grid"></i>
+                            Employee List
+                        </a>
+                    </li>
+                    
+                      
+                </ul>
+            </li>
+         </ul>
+        <!-- END SIDEBAR MENU -->
+    </div>
+</div>
